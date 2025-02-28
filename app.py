b3d5a9f2209fc0be06b2a054acf464bd5080babc
LAB_C_REAL <labc@continental.edu.pe>
2025-02-27 21:08:54 -0500

    hola

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html', title='Equisd')
+
+# Servir archivos estáticos desde la carpeta 'static'
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.root_path, filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
